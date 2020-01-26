cc30e89b0ee92641f3f8f6fb7111a7fb27b47fdd
Calvin Hutcheon <chutcheon@mica.edu>
2020-01-26 16:29:22 -0500

    continued styling

--- public/JS/style.js
+++ public/JS/style.js
@@ -1,67 +1,8 @@
-// virtues hover calls
-showinfo("#smart", "#smartedge");
-showinfo("#uncupled", "#uncouplednet");
-showinfo("#freedom", "#freedomnet");
-showinfo("#construction", "#opensourcenetnet");
 squiwtchb();
 squiwtcha();
 squiwtchc();
 
-/*
-showpos(".positionframe", "#smallnum", #smallnum);
-*/
-// scroll function
 
-var element_position = $('#trig').offset().top;
-var screen_height = $(window).height();
-var activation_offset = 0.5;//determines how far up the the page the element needs to be before triggering the function
-var activation_point = element_position - (screen_height * activation_offset);
-var max_scroll_height = $('body').height() - screen_height - 5;//-5 for a little bit of buffer
-
-//Does something when user scrolls to it OR
-//Does it when user has reached the bottom of the page and hasn't triggered the function yet
-$(window).on('scroll', function() {
-    var y_scroll_pos = window.pageYOffset;
-
-    var element_in_view = y_scroll_pos > activation_point;
-    var has_reached_bottom_of_page = max_scroll_height <= y_scroll_pos && !element_in_view;
-
-    if(element_in_view || has_reached_bottom_of_page) {
-      $("#smallnum").addClass("showposit");
-      $("#activenet").addClass("showposita");
-    }
-});
-
-
-// hover function
-
-function showinfo(parent, info) {
-  var dop = $( info );
-  var car = $( info ).find(".carrottop");
-  $( parent ).click(function() {
-      if(dop.hasClass("hover")) {
-        dop.removeClass("hover");
-      } else {
-        dop.addClass("hover");
-        car.addClass("twist");
-      }
-    })
-  };
-
-/*
-
-function showpos(parent, info, infomo) {
-  $( parent )
-    .mouseenter(function() {
-      $( info ).addClass("showposit");
-      $( infomo ).addClass("showposita");
-    })
-    .mouseleave(function() {
-      $( info ).removeClass("showposit");
-      $( infomo ).removeClass("showposita");
-    });
-}
-*/
 // perspective functiona
 
 !(function ($doc, $win) {
